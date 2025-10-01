6ecd6405426fc1e157bbd8e0722dd4a1f9743478
Pritam Behera <beherapritam2003@gmail.com>
2025-10-02 01:32:22 +0530

    added the logos to the respective platforms

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -187,9 +187,7 @@ const About = () => {
       buttonShadow: 'hover:shadow-orange-500/30',
       profileUrl: 'https://leetcode.com/u/Pritam1293/',
       logo: (
-        <svg className="w-8 h-8 text-white" viewBox="0 0 24 24" fill="currentColor">
-          <path d="M13.483 0a1.374 1.374 0 0 0-.961.438L7.116 6.226l-3.854 4.126a5.266 5.266 0 0 0-1.209 2.104 5.35 5.35 0 0 0-.125.513 5.527 5.527 0 0 0 .062 2.362 5.83 5.83 0 0 0 .349 1.017 5.938 5.938 0 0 0 1.271 1.818l4.277 4.193.039.038c2.248 2.165 5.852 2.133 8.063-.074l2.396-2.392c.54-.54.54-1.414.003-1.955a1.378 1.378 0 0 0-1.951-.003l-2.396 2.392a3.021 3.021 0 0 1-4.205.038l-.02-.019-4.276-4.193c-.652-.64-.972-1.469-.948-2.263a2.68 2.68 0 0 1 .066-.523 2.545 2.545 0 0 1 .619-1.164L9.13 8.114c1.058-1.134 3.204-1.27 4.43-.278l3.501 2.831c.593.48 1.461.387 1.94-.207a1.384 1.384 0 0 0-.207-1.943l-3.5-2.831c-.8-.647-1.766-1.045-2.774-1.202l2.015-2.158A1.384 1.384 0 0 0 13.483 0z" />
-        </svg>
+        <img src="/images/leetcode-logo.png" alt="LeetCode" className="w-8 h-8 object-contain" />
       ),
       stats: [
         {
@@ -252,7 +250,7 @@ const About = () => {
         primary: 'bg-blue-500/10',
         secondary: 'bg-cyan-500/10'
       },
-      logoGradient: 'from-blue-500 to-cyan-600',
+      logoGradient: 'from-red-500 to-cyan-600',
       logoShadow: 'shadow-blue-500/30',
       titleGradient: 'from-blue-400 via-cyan-400 to-blue-400',
       buttonGradient: 'from-blue-500 to-cyan-600',
@@ -260,9 +258,11 @@ const About = () => {
       buttonShadow: 'hover:shadow-blue-500/30',
       profileUrl: 'https://codeforces.com/profile/pritam1293',
       logo: (
-        <svg className="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 24 24">
-          <path d="M4.5 7.5C5.328 7.5 6 8.172 6 9v10.5c0 .828-.672 1.5-1.5 1.5S3 20.328 3 19.5V9c0-.828.672-1.5 1.5-1.5zm15 0c.828 0 1.5.672 1.5 1.5v10.5c0 .828-.672 1.5-1.5 1.5S18 20.328 18 19.5V9c0-.828.672-1.5 1.5-1.5zM12 3c2.21 0 4 1.79 4 4v1h1.5c.828 0 1.5.672 1.5 1.5v11c0 .828-.672 1.5-1.5 1.5h-13C3.672 21 3 20.328 3 19.5v-11C3 7.672 3.672 7 4.5 7H6V7c0-2.21 1.79-4 4-4z" />
-        </svg>
+        <div className="flex items-end justify-center gap-1">
+          <div className="w-2 h-4 bg-yellow-400 rounded-sm"></div>
+          <div className="w-2 h-6 bg-blue-500 rounded-sm"></div>
+          <div className="w-2 h-3.5 bg-red-600 rounded-sm"></div>
+        </div>
       ),
       stats: [
         {
@@ -333,9 +333,7 @@ const About = () => {
       buttonShadow: 'hover:shadow-yellow-500/30',
       profileUrl: 'https://www.codechef.com/users/pritam1293',
       logo: (
-        <svg className="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 24 24">
-          <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8 0-1.85.63-3.55 1.69-4.9L16.9 18.31C15.55 19.37 13.85 20 12 20zm6.31-3.1L7.1 5.69C8.45 4.63 10.15 4 12 4c4.41 0 8 3.59 8 8 0 1.85-.63 3.55-1.69 4.9z" />
-        </svg>
+        <img src="/images/codechef-logo.jpg" alt="CodeChef" className="w-8 h-8 object-contain rounded" />
       ),
       stats: [
         {
@@ -406,9 +404,7 @@ const About = () => {
       buttonShadow: 'hover:shadow-indigo-500/30',
       profileUrl: 'https://atcoder.jp/users/pritam1293',
       logo: (
-        <svg className="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 24 24">
-          <path d="M12 2L1 7v10c0 5.55 3.84 9.74 9 11 5.16-1.26 9-5.45 9-11V7l-11-5z" />
-        </svg>
+        <img src="/images/atcoder-logo.png" alt="AtCoder" className="w-8 h-8 object-contain" />
       ),
       stats: [
         {
